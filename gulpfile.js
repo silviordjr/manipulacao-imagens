8cd17d22ec9d7ec27ec3c4a598167fd4807acc5a
silviordjr <silviordjr@outlook.com>
2022-08-11 16:53:13 -0300

    pacotes

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const sass = require('gulp-sass')(require('sass'))
 const cssMin = require('gulp-cssimport')
 const autoprefixer = require('gulp-autoprefixer')
 const cleanCss = require('gulp-clean-css')
+const concat = require('gulp-concat')
+const babel = require('gulp-babel')
 
 function compilaSass() {
     return gulp.src('css/scss/*.scss')
@@ -20,5 +22,12 @@ function compilaCss () {
     .pipe(gulp.dest('css/css/build'))
 }
 
+function gulpConcat() {
+    return gulp.src('./js/script.js')
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('./js/build/'))
+}
+
 gulp.task('sass', compilaSass)
 gulp.task('css', compilaCss)
+gulp.task('js', gulpConcat)
